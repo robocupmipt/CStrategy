3f626ea2af1fb25b6e2b09eb849de249c4662b61
dimakarp1996 <dimakarp1996@yandex.ru>
2019-02-19 15:35:13 +0300

    Update KernelStrategy.hpp

--- KernelStrategy.hpp
+++ KernelStrategy.hpp
@@ -10,6 +10,10 @@
 #include <atomic>
 #include <thread>
 #include <chrono>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 namespace strategy {
     struct Command {
