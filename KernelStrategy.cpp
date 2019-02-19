0f7ca59adc4d7b9ce29c481f31db939383f7dbda
dimakarp1996 <dimakarp1996@yandex.ru>
2019-02-19 15:35:00 +0300

    Update KernelStrategy.cpp

--- KernelStrategy.cpp
+++ KernelStrategy.cpp
@@ -3,6 +3,11 @@
 //
 
 #include "KernelStrategy.hpp"
+#include <vector>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 
 strategy::Robot KernelStrategy::GetLeader() {
